522c544d352cf5f125ddf0c8ac27a0286c45925f
bnfrsMusic <sahoo.ayush@gmail.com>
2024-09-17 11:12:15 -0400

    Update loss.rs

--- src/lib/loss.rs
+++ src/lib/loss.rs
@@ -10,23 +10,15 @@ pub const MSE: LossFunction = LossFunction {
     name: "Mean Squared Error",
     function: |predicted: &Tensor, actual: &Tensor| {
         //Function
+
+        //MSE = 1/n Sum[i=1] (actual - predicted)^2
         let mut diff = predicted.clone();
-        diff.substract(actual);
-        diff.dot(&diff);
-        diff.data.iter().sum::<f64>() / (predicted.data.len() as f64)
+
+        //(actual - predicted)^2
+        diff.substract(actual); //calculates the difference
+        diff.dot(&diff); //Squares the difference
+
+        //Sum[i=1] / n
+        diff.data.iter().sum::<f64>() / (predicted.data.len() as f64) //
     },
 };
-
-//pub const MSE: LossFunction = LossFunction {
-//    name: "Mean Squared Error",
-//    function: |predicted: &Tensor, actual: &Tensor| {
-//        let diff = predicted.substract(actual);
-//        let squared = diff.dot(&diff);
-//        squared.data.iter().sum::<f64>() / (predicted.data.len() as f64)
-//    },
-//    derivative: |predicted: &Tensor, actual: &Tensor| {
-//        let mut diff = predicted.substract(actual);
-//        let scale = 2.0 / (predicted.data.len() as f64);
-//        diff.map(&|x| x * scale)
-//    },
-//};
